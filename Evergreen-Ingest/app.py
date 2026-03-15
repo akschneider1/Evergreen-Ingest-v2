222710dd8a44caf785522e75b44542d6ca27ce98
Alexander Schneider <akschneider1@gmail.com>
2026-03-15 02:11:07 +0000

    Run command

--- Evergreen-Ingest/app.py
+++ Evergreen-Ingest/app.py
@@ -413,4 +413,5 @@ async def report_json(comparison_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
